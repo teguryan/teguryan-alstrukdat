9f5bac1f9fef104876fb0b5fc216284beda8eda4
teguryan <110823709+teguryan@users.noreply.github.com>
2024-03-14 18:11:53 +0700

    Update soal.c

--- soal.c
+++ soal.c
@@ -30,12 +30,12 @@ int checkLenghtRequirement(char* text){
 }
 
 int main() {
-    int lengthOfText, selectOption;
+    int blengthOfText, lengthOfText, selectOption;
     FILE *fptr = NULL;
     char text[MAX_LENGTH];
 
     fptr = fopen("file.txt", "r");
-
+    
     if(fptr == NULL){
         printf("Error");
         exit(1);
@@ -45,13 +45,26 @@ int main() {
 
     fclose(fptr);
 
-    lengthOfText = strlen(text);
-    printf("Leght before : %d\n",lengthOfText);
-
     selectOption = checkLenghtRequirement(text);
     void (*fun_ptr_arr[])(int*) = {lessThanRequired, equalThanRequired, moreThanRequired};
     (*fun_ptr_arr[selectOption])(&lengthOfText);
+    blengthOfText = strlen(text);
+    printf("Leght before : %d\n",blengthOfText);
     printf("The Lenght is updated to %d", lengthOfText);
 
     return 0;
 }
+
+// program berjalan dengan mendeklarasikan variable blenghtOfTextlenghtOfText dan selectOption terlebih dahulu
+// lalu FILE *fptr = NULL untuk deklarasi sebuah file yang akan dipanggil nantinya dengan nilai null agar nilai variable fptr NULL
+// lanjut dengan deklarasi sebuah variable text dengan panjang maxlength, program disini telah mendeklarasikan panjang (beserta nilai) max dan min diawal code dengan #define
+// lalu step berikutnya membuka file dengan code atau syntax fptr = fopen("nama file","r") r disini untuk read jadi diperuntukan untuk dibaca saja
+// pengondisian jika fptr null maka akan program akan keluar dan mengirim pesan "Error"
+// fgets disni untuk mengambil nilai dari file text, dengan panjang maxlenght lalu fptr untuk tujuannya
+// lalu fclose untuk menutup file
+// selectOption disini untuk menampung function check
+// void funptrarr(function pointer array) untuk menyimpan function2 pengecekan yang telah di buat diatas program main
+// lalu variable fun_ptr_arr tadi akan melakukan pengecekan melalui selectOption dengan panjang text yang didalam file lalu dicetak apakah nilai masuk kedalam function less/equal/more
+// nantinya program akan mengeluarkan hasil sesuai function yang terpilih
+// setelah itu blengOfText berbeda dengan lenghtOfText,disini b(before) ntuk menginput berapa banyak character yang ada didalam file, kalau lenghtOfText untuk mencetak apakah kondisi kurang dari 1945, atau malah kelebihan dari 2024 atau masuk diantaranya
+// lalu diprintkan hasil nya melalui output printf
